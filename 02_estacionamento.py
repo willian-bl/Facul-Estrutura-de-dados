9f033f05d80c8030c308db3e11cd19a51817f982
Willian <willianbrtl@hotmail.com>
2023-03-18 22:34:42 -0300

    Aula 13/03

---
+++ 02_estacionamento.py
@@ -0,0 +1,127 @@
+"""
+Exercício - Estacionamento
+
+Um estacionamento possui uma única alameda para guardar os carros que serão
+estacionados. Existe apenas uma entrada/saída no estacionamento, em uma
+extremidade da alameda. Se chegar um cliente para retirar um carro que não seja o
+mais próximo da saída, todos os carros bloqueando seu caminho sairão do
+estacionamento, o carro do cliente será manobrado para fora do estacionamento, e
+os outros carros voltarão a ocupar a mesma sequência inicial. Escreva um programa
+em python que processe um conjunto de entradas. Cada entrada contém um 'E', de
+entrada, ou um 'S', de saída, e o número da placa do carro. Presume-se que os
+carros cheguem e partam na ordem que chegaram no estacionamento. O
+programa deve imprimir uma mensagem sempre que um carro sair do
+estacionamento. Quando um carro sair do estacionamento, a mensagem deverá
+incluir o número de vezes em que o carro foi manobrado para fora do
+estacionamento para permitir que outros carros saíssem.
+
+"""
+
+
+from os import system
+from funcoes_pilhas import *
+from time import sleep
+
+class Veiculo:  # No estacionamento (representado por uma pilha), serão armazenados objetos do tipo Veiculo
+    def __init__(self, placa):
+        self.placa = placa
+        self.manobras = 0
+
+
+def entradaEstacionamento(estacionamento, placa_veiculo):
+    if estaEstacionado(estacionamento, placa_veiculo):
+        print('Já existe um veículo no estacionamento com essa placa!')
+        return
+
+    v = Veiculo(placa=placa_veiculo)
+    push(estacionamento, v)
+    print(f'Veículo com a placa {placa_veiculo} adicionado no estacionamento!')
+
+    
+def estaEstacionado(estacionamento, placa_veiculo):
+    """Verifica se um veículo com determinada placa está no estacionamento"""
+    aux = []
+    estacionado = False
+
+    while not vazia(estacionamento):
+        v = pop(estacionamento)
+        push(aux, v)
+
+        if v.placa == placa_veiculo:
+           estacionado = True
+           break
+
+    while not vazia(aux):
+        push(estacionamento, pop(aux))
+
+    return estacionado 
+        
+
+def saidaEstacionamento(estacionamento, placa_veiculo):
+    if vazia(estacionamento):
+        print('Estacionamento vazio!')
+        return
+    
+    if not estaEstacionado(estacionamento, placa_veiculo):
+        print('O veículo com a placa informada não está no estacionamento!')
+        return
+    
+    aux = []
+    while not vazia(estacionamento):
+        v = pop(estacionamento)
+        if v.placa == placa_veiculo:
+            print(f'Veículo retirado do estacionamento com sucesso! \nPlaca: {v.placa} | Nº de Manobras: {v.manobras}')
+            break
+        push(aux, v)
+    
+    while not vazia(aux):
+        v = pop(aux)
+        v.manobras += 1
+        push(estacionamento, v)
+
+
+def printaEstacionamento(estacionamento):
+    print('\nEstacionamento agora:')
+    if vazia(estacionamento):
+        print('O estacionamento está vazio! \n')
+        return
+
+    aux = []
+    print()
+    while not vazia(estacionamento):
+        v = pop(estacionamento)
+        print('|', v.placa, '|')
+        push(aux, v)
+    print('=' * (len(v.placa) + 4), '\n')
+
+    while not vazia(aux):
+        push(estacionamento, pop(aux))
+
+
+estacionamento = []
+
+while True:
+    system('cls')
+    
+    print("===================================== Estacionamento da parada =====================================")
+    print('Bem vindo ao estacionamento! \nPara adicionar um veículo, digite "E" seguido do número da placa. Ex: "E ABC1234" \nPara retirar um veículo, digite "S" seguido do número da placa. Ex: "S ABC1234" \nEnvie uma entrada vazia para sair do programa')
+    
+    printaEstacionamento(estacionamento)
+
+    x = input('> ').strip()
+    
+    if x.lower().startswith('e'):
+        entradaEstacionamento(estacionamento, placa_veiculo=x[2:])
+
+    elif x.lower().startswith('s'):
+        saidaEstacionamento(estacionamento, placa_veiculo=x[2:])
+    
+    elif x == '':
+        print('Espero que esteja satisfeito com nossos serviços, até a próxima!')
+        sleep(3)
+        break
+
+    else:
+        print('\nDigite algo válido! Caso queira sair, apenas pressione Enter, sem digitar nada')
+
+    sleep(3)
---
+++ funcoes_pilhas.py
@@ -0,0 +1,143 @@
+"""Arquivo com funções usadas para manipulação de pilhas. Conforme novas funções forem sendo criadas durante as aulas, elas serão adicionadas aqui para serem importadas em outros projetos"""
+
+# ------------------------------ FUNÇÕES PRIMITIVAS ------------------------------
+def push(p, v):
+    """Coloca um valor no topo da pilha"""
+    # p representa a pilha (utilizamos uma lista para representar) e v o valor a ser inserido
+    p.append(v)
+
+
+def pop(p):
+    """Retira um valor do topo da pilha"""
+    return p.pop()
+
+
+def vazia(p):
+    """Verifica se a pilha está vazia ou não. Caso esteja, retorna `True`"""
+    return False if p else True
+
+# ------------------------------ FUNÇÕES COMPLEMENTARES ------------------------------
+def mostraPilha(p):
+    """
+    A função mostrapilha tem como objetivo mostrar o conteudo da \n
+    pilha p (passada como parâmetro) utilizando as funções primitivas \n
+    para manipulação de pilha.
+    """
+
+    if vazia(p):
+        print('Pilha vazia!')
+        return
+
+    aux = [] # Para a elaboração da função precisa-se de uma pilha auxiliar
+    while not vazia(p): # enquanto a pilha p não estiver vazia
+        v = pop(p) # retirar o elemento do topo da pilha P
+        push(aux, v) # colocar o elemento na pilha auxiliar
+    
+    while not vazia(aux):   # retornar os elementos da pilha auxiliar
+        v = pop(aux)        # para a pilha P
+        print(v)            # mostrar os elementos na ordem de inserção
+        push(p, v)
+
+
+def quantidadePilha(p):
+    """
+    A função quantidadePilha tem como objetivo contar a quantidade de valores armazenados \n
+    em uma pilha p (passada como parâmetro) utilizando as funções primitivas \n
+    para manipulação de pilha e mantendo os elementos na pilha original
+    """
+
+    aux = [] # Para a elaboração da função precisa-se de uma pilha auxiliar
+    qe = 0  # E de um contador
+    while not vazia(p): # enquanto a pilha p não estiver vazia
+        v = pop(p) # retirar o elemento do topo da pilha P
+        push(aux, v) # colocar o elemento na pilha auxiliar
+    
+    while not vazia(aux):   # retornar os elementos da pilha auxiliar
+        v = pop(aux)        # para a pilha P
+        qe += 1      # e contar os elementos equanto isso é feito
+        push(p, v)
+    
+    return qe
+
+
+def retiraPrimeiro(p):
+    """
+    Função para retirar o primeiro valor inserido em uma lista \n
+    Passa todos os elementos para uma pilha auxiliar, depois dá um pop nessa pilha \n
+    e retorna os elementos restantes para a pilha, sem o primeiro.
+    """
+    
+    aux = []
+    
+    if vazia(p):  # Verifica se a pilha está vazia, pois se estiver não tem nada para retirar
+        print('Pilha vazia')
+        return
+
+    while not vazia(p):     # Coloca os elementos da pilha p em uma pilha auxiliar, pois assim os elementos na pilha auxiliar
+        push(aux, pop(p))   # estarão na ordem inversa aos da pilha p, sendo que o primeiro elemento colocado em p será o elemento
+                            # do topo de aux, ao qual temos acesso.
+    
+    primeiro = pop(aux)  # Agora, retira o elemento do topo da pilha auxiliar
+
+    while not vazia(aux):   # Por fim, retornamos os elementos de auxiliar para p, mas sem o primeiro valor, que demos
+        push(p, pop(aux))   # o pop antes
+        
+    return primeiro
+
+
+def copiaPilha(p1, p2):
+    """Função que faz uma cópia idêntica de uma pilha para outra, com os elementos na mesma ordem que foram inseridos na pilha original"""
+
+    aux = []
+
+    while not vazia(p1):  # Retira os elementos da pilha p1 e coloca na pilha auxiliar
+        push(aux, pop(p1))
+
+    while not vazia(aux):  
+        v = pop(aux)    # Agora, retira os elementos da pilha auxiliar e coloca
+        push(p1, v)     # tanto na pilha p1
+        push(p2, v)     # quanto na pilha p2
+
+
+def pilhas_iguais(p1, p2):
+    """
+    Compara duas pilhas e retorna True caso elas sejam iguais. \n
+    Só retornará True caso as pilhas tenham o mesmo tamanho e os mesmos elementos na mesma ordem.
+    """
+    
+    if quantidadePilha(p1) != quantidadePilha(p2):  # Se o tamanho das pilhas não for igual, não precisamos nem comparar,
+        return False                                # já retornamos false e não fazemos o resto
+
+    # Caso o tamanho das pilhas seja igual, fazemos as comparações
+    p_aux1, p_aux2 = [], []
+    eh_igual = True  # Começamos assumindo que as pilhas são iguais 
+
+    # Comparando os valores
+    while not vazia(p1):  # Como as pilhas tem tamanho igual, quando os elementos da pilha 1 se esgotarem, os da pilha 2 também terão se esgotado
+        
+        # Retira das pilhas originais
+        v1 = pop(p1)
+        v2 = pop(p2)
+
+        # Coloca nas pilhas auxiliares
+        push(p_aux1, v1)
+        push(p_aux2, v2)
+
+        if v1 != v2:            # Caso o valor retirado de uma pilha seja diferente do valor retirado da outra, as pilhas não são iguais
+            eh_igual = False    # Mudamos o valor do flag, que foi definido como True por padrão
+            break               # Não precisamos comparar mais
+    
+    # Retornando valores para as pilhas originais
+    while not vazia(p_aux1):
+        
+        # Retira das pilhas auxiliares
+        v1 = pop(p_aux1)
+        v2 = pop(p_aux2)
+
+        # Coloca nas pilhas originais
+        push(p1, v1)
+        push(p2, v2)
+    
+    return eh_igual
+
+
